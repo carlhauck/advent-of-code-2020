60793872953438954bbdac48bd139a96e4dd5998
carlhauck <carlhauck@gmail.com>
2020-12-09 12:42:31 -0600

    adds day 7 part 2 solution

--- day-7-handy-haversacks2.rb
+++ day-7-handy-haversacks2.rb
@@ -1,32 +1,28 @@
-# --- Day 7: Handy Haversacks ---
-# You land at the regional airport in time for your next flight. In fact, it looks like you'll even have time to grab some food: all flights are currently delayed due to issues in luggage processing.
+# --- Part Two ---
+# It's getting pretty expensive to fly these days - not because of ticket prices, but because of the ridiculous number of bags you need to buy!
 
-# Due to recent aviation regulations, many rules (your puzzle input) are being enforced about bags and their contents; bags must be color-coded and must contain specific quantities of other color-coded bags. Apparently, nobody responsible for these regulations considered how long they would take to enforce!
+# Consider again your shiny gold bag and the rules from the above example:
 
-# For example, consider the following rules:
+# faded blue bags contain 0 other bags.
+# dotted black bags contain 0 other bags.
+# vibrant plum bags contain 11 other bags: 5 faded blue bags and 6 dotted black bags.
+# dark olive bags contain 7 other bags: 3 faded blue bags and 4 dotted black bags.
+# So, a single shiny gold bag must contain 1 dark olive bag (and the 7 bags within it) plus 2 vibrant plum bags (and the 11 bags within each of those): 1 + 1*7 + 2 + 2*11 = 32 bags!
 
-# light red bags contain 1 bright white bag, 2 muted yellow bags.
-# dark orange bags contain 3 bright white bags, 4 muted yellow bags.
-# bright white bags contain 1 shiny gold bag.
-# muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
-# shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
-# dark olive bags contain 3 faded blue bags, 4 dotted black bags.
-# vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
-# faded blue bags contain no other bags.
-# dotted black bags contain no other bags.
-# These rules specify the required contents for 9 bag types. In this example, every faded blue bag is empty, every vibrant plum bag contains 11 bags (5 faded blue and 6 dotted black), and so on.
+# Of course, the actual rules have a small chance of going several levels deeper than this example; be sure to count all of the bags, even if the nesting becomes topologically impractical!
 
-# You have a shiny gold bag. If you wanted to carry it in at least one other bag, how many different bag colors would be valid for the outermost bag? (In other words: how many colors can, eventually, contain at least one shiny gold bag?)
+# Here's another example:
 
-# In the above rules, the following options would be available to you:
+# shiny gold bags contain 2 dark red bags.
+# dark red bags contain 2 dark orange bags.
+# dark orange bags contain 2 dark yellow bags.
+# dark yellow bags contain 2 dark green bags.
+# dark green bags contain 2 dark blue bags.
+# dark blue bags contain 2 dark violet bags.
+# dark violet bags contain no other bags.
+# In this example, a single shiny gold bag must contain 126 other bags.
 
-# A bright white bag, which can hold your shiny gold bag directly.
-# A muted yellow bag, which can hold your shiny gold bag directly, plus some other bags.
-# A dark orange bag, which can hold bright white and muted yellow bags, either of which could then hold your shiny gold bag.
-# A light red bag, which can hold bright white and muted yellow bags, either of which could then hold your shiny gold bag.
-# So, in this example, the number of bag colors that can eventually contain at least one shiny gold bag is 4.
-
-# How many bag colors can eventually contain at least one shiny gold bag? (The list of rules is quite long; make sure you get all of it.)
+# How many individual bags are required inside your single shiny gold bag?
 
 puzzle_input = "bright indigo bags contain 4 shiny turquoise bags, 3 wavy yellow bags.
 dotted turquoise bags contain 3 vibrant salmon bags, 2 dotted maroon bags, 1 bright beige bag, 1 drab white bag.
@@ -623,13 +619,22 @@ shiny lime bags contain 4 striped olive bags, 3 dim coral bags.
 striped indigo bags contain 3 wavy red bags, 5 posh white bags, 5 light tan bags, 1 plaid bronze bag.
 wavy crimson bags contain 2 dull fuchsia bags, 5 striped tomato bags."
 
+# puzzle_input = "light red bags contain 1 bright white bag, 2 muted yellow bags.
+# dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+# bright white bags contain 1 shiny gold bag.
+# muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+# shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+# dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+# vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+# faded blue bags contain no other bags.
+# dotted black bags contain no other bags."
 
-def count_shiny_gold_containers(bag_rules)
+
+def count_bags_inside(bag_rules)
   bag_rules = bag_rules.split(/\n/)
-  rules_ary = []
+  @rules_hash = {}
   compatibles = []
   bag_rules.each do |rule|
-    outside_hash = {}
     inside_hash = {}
     edit = rule.chop.gsub(" bags", '').gsub(" bag", '')
     edit = edit.split(" contain ")
@@ -643,30 +648,25 @@ def count_shiny_gold_containers(bag_rules)
       end
     end
     if inside_bags != ["no other"] # removes bags that contain nothing
-      outside_hash[outside_bag] = inside_hash
-      rules_ary << outside_hash
-      if inside_bags.include?("shiny gold")
-        compatibles << outside_bag
-      end
+      @rules_hash[outside_bag] = inside_hash
     end
   end
-  while true
-    compatibles_start = compatibles.dup
-    rules_ary.each {|k,v|
-      key = k.keys[0]
-      values = k.values[0]
-      values.each {|value|
-        if compatibles.include?(value[0])
-          compatibles << key
-        end
-      }
-    }
-    compatibles.uniq!
-    if compatibles_start.count == compatibles.count
-      break
-    end
-  end
-  compatibles.count
+  
+  @sum = 0
+  check_bag_contents("shiny gold")
+
+  @sum
 end
 
-p count_shiny_gold_containers(puzzle_input)
+def check_bag_contents(bag)
+  if @rules_hash[bag]
+    @rules_hash[bag].each {|k, v|
+      @sum += v.to_i
+      v.to_i.times do
+        check_bag_contents(k)
+      end
+    }
+  end
+end
+
+p count_bags_inside(puzzle_input)
